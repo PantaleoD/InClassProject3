7a7f9ae1fcec7326d8bb60ba5f8bb5295aa535db
unknown <dpantaleo@ttc-2160-20.kvcc.dom>
2015-10-19 16:16:45 -0400

    to pg 137

---
+++ app/src/main/java/edu/kvcc/androidstudioprojects/inclass3/inclass3/Crime.java
@@ -0,0 +1,34 @@
+package edu.kvcc.androidstudioprojects.inclass3.inclass3;
+
+import java.util.UUID;
+
+/**
+ * Created by dpantaleo on 10/19/2015.  Rt Click on java class - new java class
+ */
+public class Crime {
+
+    //   this will be unique even if names are same;
+    private UUID mID;         // to distinguish 1 crime from another   (type is UUID (unique identifier) (string value))
+                                // if from a db - replaces with db id #
+    private  String mTitle;    //
+
+       public Crime(){      // default constructor
+
+           mID = UUID.randomUUID();    // makes anew unique id for this particular crime
+       }
+
+    // getters and setters: - rt click - generate - getters & setters
+
+
+    public UUID getmID() {    // kept getter deleted setter+
+        return mID;
+    }
+
+        public String getmTitle() {     // need getter and setter for the title
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
--- app/src/main/java/edu/kvcc/androidstudioprojects/inclass3/inclass3/CrimeActivity.java
+++ app/src/main/java/edu/kvcc/androidstudioprojects/inclass3/inclass3/CrimeActivity.java
@@ -1,11 +1,12 @@
 package edu.kvcc.androidstudioprojects.inclass3.inclass3;
 
+import android.support.v4.app.FragmentActivity;   // this was handled in the gradle app
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +14,18 @@ public class CrimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_crime);
     }
 
-    @Override
+
+
+
+}
+
+
+
+
+
+
+
+ /*   @Override                                       NA so removed
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_crime, menu);
@@ -33,5 +45,5 @@ public class CrimeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-}
+    }  */
+
